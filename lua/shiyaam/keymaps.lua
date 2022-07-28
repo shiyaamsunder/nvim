5fdd5d17234a7c90a1d1459c2c7427650eb12601
shiyaam <shiyaamsunderms@gmail.com>
2022-07-28 19:58:03 +0530

    misc: removed comments

--- lua/shiyaam/keymaps.lua
+++ lua/shiyaam/keymaps.lua
@@ -1,17 +1,9 @@
 local map = function(mode, lhs, rhs)
-          vim.api.nvim_set_keymap(mode, lhs, rhs, {noremap = true, silent = true})
+    vim.api.nvim_set_keymap(mode, lhs, rhs, { noremap = true, silent = true })
 end
 
 
 -- Normal
 map("n", "<C-p>", ":Files<CR>")
 map("n", "<Leader>ck", ":tabe ~/.config/nvim/lua/shiyaam/keymaps.lua<CR>")
-map("n", "<Leader>tt" ,":tabclose<CR>")
-
--- Insert
-
-
--- Terminal
--- TODO: Add terminal commands
-
-
+map("n", "<Leader>tt", ":tabclose<CR>")
