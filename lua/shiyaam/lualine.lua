f62ab32f57a6cfe2fcde01612db96f80b252ee8a
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 00:04:57 +0530

    (fix): added install check

--- lua/shiyaam/lualine.lua
+++ lua/shiyaam/lualine.lua
@@ -1,7 +1,11 @@
-require('lualine').setup {
-  -- options = {
-  --     theme='gruvbox-material'
-  -- },
+local status_ok, lualine = pcall(require, "lualine")
+if not status_ok then
+  return
+end
+lualine.setup {
+  options = {
+      theme='ayu'
+  },
   tabline = {
     lualine_a = {
       {
