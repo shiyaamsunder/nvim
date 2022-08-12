072669b64c28dbd70b443b22393f2bfd1c926fae
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 00:01:40 +0530

    (options): new options

--- lua/shiyaam/options.lua
+++ lua/shiyaam/options.lua
@@ -1,16 +1,39 @@
-local opt = vim.opt
-local g = vim.g
 
-vim.cmd([[let mapleader=" "]])
-opt.relativenumber = true
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.signcolumn = "yes:1"
+local options = {
+  background="dark",
+  --clipboard="unnamedplus",      -- allows neovim to access the system clipboard
+  cmdheight=1,                  -- more space in neovim command line
+  conceallevel=0,               -- so that `` is visbile in markdown files
+  cursorline = true,            -- highlights the current line the cursor is on
+  hlsearch=true,                -- highlight all matches on previous search
+  ignorecase=true,              -- ignore case in search patterns
+  relativenumber=true,
+  showtabline=2,                -- always show the tabs
+  signcolumn="yes:1",
+  termguicolors = true,
+  swapfile=false,               -- stops creating swapfile
+  undofile=true,                -- persistent undo
+  updatetime=100,               -- faster completion
+  wrap=false,                   -- disables wrap
+  sidescrolloff=8,
+  title=true,
+  fileencoding = "utf-8",       -- the encoding written to a file
+  completeopt = { "menuone", "noselect" },
 
-opt.termguicolors = true
-opt.background = 'dark'
-g.gruvbox_material_better_performance = 1
+  -- Tabs and Spaces
+  expandtab=true,               -- convert spaces to tabs
+  shiftwidth=2,                 -- no of spaces to be inserted for indentation
+  tabstop=2,                    -- 2 spaces for a tab
+  smartindent=true,             -- Do smart autoindenting when starting a new line.
+
+  --Splits
+  splitbelow=true,
+  splitright=true
+}
+
+vim.g.gruvbox_material_better_performance = 1
 vim.cmd([[ colorscheme gruvbox-material]])
--- vim.cmd([[ colorscheme oxocarbon]])
-vim.cmd([[ command! MakeTags !ctags -R . ]])
+vim.cmd "set whichwrap+=<,>,h,l,[,]"
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
