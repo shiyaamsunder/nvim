945489c3d78270935cbafcee7b7f1acac715e6f4
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 00:04:44 +0530

    (fix): server_capabilities

--- lua/shiyaam/lsp.lua
+++ lua/shiyaam/lsp.lua
@@ -25,7 +25,7 @@ local on_attach = function(client, bufnr)
 
   buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
 
-  if client.resolved_capabilities.document_formatting then
+  if client.server_capabilities.document_formatting then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
     vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
