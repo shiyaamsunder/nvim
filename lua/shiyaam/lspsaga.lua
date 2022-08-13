bbe852ef6cc2bb8eecc3b212ac0e2741b2e37350
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 11:42:24 +0530

    (remap): Lspa

--- lua/shiyaam/lspsaga.lua
+++ lua/shiyaam/lspsaga.lua
@@ -5,14 +5,13 @@ local map = function(mode, lhs, rhs)
 end
 
 map("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
-map("n", "gr", "<cmd>Lspsaga rename<CR>")
+map("n", "<Leader>r", "<cmd>Lspsaga rename<CR>")
 map("n", "gs", "<cmd>Lspsaga signature_help<CR>")
 map("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 map("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>")
 map("n", "[e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 map("n", "]e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
 map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-map("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>")
 
 
 -- scroll down hover doc or scroll in definition preview
