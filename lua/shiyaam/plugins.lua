71ac85b1aeda58e1f65d2a6bb7b9fb054af404b2
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 11:41:28 +0530

    (misc): disabled auto packersync

--- lua/shiyaam/plugins.lua
+++ lua/shiyaam/plugins.lua
@@ -18,12 +18,12 @@ end
 
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-augroup packer_user_config
-autocmd!
-autocmd BufWritePost plugins.lua source <afile> | PackerSync
-augroup end
-]]
+-- vim.cmd [[
+-- augroup packer_user_config
+-- autocmd!
+-- autocmd BufWritePost plugins.lua source <afile> | PackerSync
+-- augroup end
+-- ]]
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
@@ -33,8 +33,9 @@ end
 
 -- Have packer use a popup window
 packer.init {
-	max_jobs=1,
-	display = {
+  snapshot_path = fn.stdpath "config" .. "/snapshots",
+  max_jobs = 1,
+  display = {
     open_fn = function()
       return require("packer.util").float { border = "rounded" }
     end,
@@ -46,62 +47,61 @@ packer.init {
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
 
   --My plugins
   -- Lua Dev
-  use "folke/lua-dev.nvim"                                        -- NVIM API lua completion
-  use "nvim-lua/plenary.nvim"                                     -- Useful lua functions used by lots of plugins
-  use "nvim-lua/popup.nvim"                                       -- Lua API for popup in vim
+  use "folke/lua-dev.nvim"                                                -- NVIM API lua completion
+  use "nvim-lua/plenary.nvim"                                             -- Useful lua functions used by lots of plugins
+  use "nvim-lua/popup.nvim"                                               -- Lua API for popup in vim
 
   -- LSP
-  use "williamboman/nvim-lsp-installer"                          -- for installing lsp servers quickly
-  use "neovim/nvim-lspconfig"                                    -- quickstart configs for lsp
-  use "jose-elias-alvarez/null-ls.nvim"                          -- for formatters and linters
-  use "RRethy/vim-illuminate"                                    -- For highlighting the other uses of current word
+  -- use "williamboman/nvim-lsp-installer"                                  -- for installing lsp servers quickly
+  use "neovim/nvim-lspconfig"                                             -- quickstart configs for lsp
+  use "jose-elias-alvarez/null-ls.nvim"                                   -- for formatters and linters
+  use "RRethy/vim-illuminate"                                             -- For highlighting the other uses of current word
+  use { "williamboman/mason.nvim" }
+  use "williamboman/mason-lspconfig.nvim"
 
   -- Autocompletion
-  use 'hrsh7th/nvim-cmp'                                          -- Completion plugin
-  use "hrsh7th/cmp-buffer"                                        -- buffer completions
-  use "hrsh7th/cmp-path"                                          -- path completions
-  use "hrsh7th/cmp-cmdline"                                       -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip"                                  -- snippet completions
-  use 'hrsh7th/cmp-nvim-lsp'
-
-  use 'lewis6991/impatient.nvim'                                  -- Plugin to load neovim faster
-  use "lukas-reineke/indent-blankline.nvim"
+  use "hrsh7th/nvim-cmp"                                                  -- Completion plugin
+  use "hrsh7th/cmp-buffer"                                                -- buffer completions
+  use "hrsh7th/cmp-path"                                                  -- path completions
+  use "hrsh7th/cmp-cmdline"                                               -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"                                          -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
 
   -- File explorer
   use {
-    'kyazdani42/nvim-tree.lua',                                   -- Nvim Tree, File explorer
+    "kyazdani42/nvim-tree.lua",                                           -- Nvim Tree, File explorer
     requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+      "kyazdani42/nvim-web-devicons", -- optional, for file icons
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
 
 
   -- Fuzzy Finder
-  use {                                                           -- Fuzzy Finder binary
+  use {                                                                   -- Fuzzy Finder binary
     'junegunn/fzf',
     run = function() vim.fn['fzf#install']() end
   }
-  use 'junegunn/fzf.vim'                                          -- Fzf vim plugin
+  use 'junegunn/fzf.vim'                                                  -- Fzf vim plugin
 
-  use { 'windwp/nvim-autopairs',                                  -- Auto closing for braces and quotes
-  config = function() require("nvim-autopairs").setup {} end
+  use { 'windwp/nvim-autopairs',                                          -- Auto closing for braces and quotes
+    config = function() require("nvim-autopairs").setup {} end
   }
 
   use {
-    'nvim-lualine/lualine.nvim',                                  -- Bottom status bar
+    'nvim-lualine/lualine.nvim',                                          -- Bottom status bar
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use {
-    "glepnir/lspsaga.nvim",                                       -- UI for autocompletion, diagnostics, docs
+    "glepnir/lspsaga.nvim",                                               -- UI for autocompletion, diagnostics, docs
     branch = "main"
   }
   use {
-    'nvim-treesitter/nvim-treesitter',                            -- Good 'ol treesitter for parsing programming languages
+    'nvim-treesitter/nvim-treesitter',                                    -- Good 'ol treesitter for parsing programming languages
     run = ':TSUpdate'
   }
   use {
@@ -118,18 +118,27 @@ return require('packer').startup(function(use)
   }
 
   --Snippet
-  use 'L3MON4D3/LuaSnip'                                          -- Snippet engine
+  use 'L3MON4D3/LuaSnip'                                                  -- Snippet engine
   use "rafamadriz/friendly-snippets"
 
   -- Colorschemes
-  use 'sainnhe/gruvbox-material'                                  -- Colorscheme 
+  use 'sainnhe/gruvbox-material' -- Colorscheme
   use { "ellisonleao/gruvbox.nvim" }
   use 'B4mbus/oxocarbon-lua.nvim'
   use "Shatur/neovim-ayu"
 
   -- Misc
-  use 'wakatime/vim-wakatime'                                     -- Code stats
-
+  use 'wakatime/vim-wakatime'                                             -- Code stats
+  use "lewis6991/impatient.nvim"                                          -- Plugin to load neovim faster
+  use "lukas-reineke/indent-blankline.nvim"
+                                                                            -- Hop for easy vim motions
+  use {
+    "phaazon/hop.nvim",
+    branch = 'v2', -- optional but strongly recommended
+  }
+  use {
+    'goolord/alpha-nvim',                                                   -- Startup Screen
+  }
 
 
   -- Automatically set up your configuration after cloning packer.nvim
