15722482887d3312d2499ac25f3aed6b0ae2c5b8
shiyaam <shiyaamsunderms@gmail.com>
2022-08-13 11:42:53 +0530

    (style): formatted

--- lua/shiyaam/lualine.lua
+++ lua/shiyaam/lualine.lua
@@ -1,27 +1,21 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
-lualine.setup {
-  options = {
-      theme='ayu'
-  },
-  tabline = {
-    lualine_a = {
-      {
-        'tabs',
-        max_length = vim.o.columns,
-        mode = 1,
-        tabs_color = {
-
-        }
-      }
-    },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = {}
-  }
-
-}
+lualine.setup({
+	tabline = {
+		lualine_a = {
+			{
+				"tabs",
+				max_length = vim.o.columns,
+				mode = 1,
+				tabs_color = {},
+			},
+		},
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+})
