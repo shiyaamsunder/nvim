c50c2931194ecb745a8ef0e7995156be587ff49c
shiyaam <shiyaamsunder@users.noreply.github.com>
2022-08-20 13:55:09 +0530

    (plugin): git signs

--- lua/shiyaam/plugins.lua
+++ lua/shiyaam/plugins.lua
@@ -1,73 +1,73 @@
 local fn = vim.fn
 
 -- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 ---@diagnostic disable-next-line: missing-parameter
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
-
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
+vim.cmd([[
 augroup packer_user_config
 autocmd!
 autocmd BufWritePost plugins.lua source <afile> | PackerInstall
 augroup end
-]]
+]])
 
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
-packer.init {
-  snapshot_path = fn.stdpath "config" .. "/snapshots",
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
-  git = {
-    clone_timeout = 300
-  }
-}
+packer.init({
+	snapshot_path = fn.stdpath("config") .. "/snapshots",
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+	git = {
+		clone_timeout = 300,
+	},
+})
 
+---@format disable
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use "wbthomason/packer.nvim"
 
   --My plugins
   -- Lua Dev
-  use "folke/lua-dev.nvim" -- NVIM API lua completion
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
-  use "nvim-lua/popup.nvim" -- Lua API for popup in vim
+  use "folke/lua-dev.nvim"                                                        -- NVIM API lua completion
+  use "nvim-lua/plenary.nvim"                                                     -- Useful lua functions used by lots of plugins
+  use "nvim-lua/popup.nvim"                                                       -- Lua API for popup in vim
 
   -- LSP
-  -- use "williamboman/nvim-lsp-installer"                                  -- for installing lsp servers quickly
-  use "neovim/nvim-lspconfig" -- quickstart configs for lsp
-  use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
-  use "RRethy/vim-illuminate" -- For highlighting the other uses of current word
+  -- use "williamboman/nvim-lsp-installer"                                          -- for installing lsp servers quickly
+  use "neovim/nvim-lspconfig"                                                     -- quickstart configs for lsp
+  use "jose-elias-alvarez/null-ls.nvim"                                           -- for formatters and linters
+  use "RRethy/vim-illuminate"                                                     -- For highlighting the other uses of current word
   use { "williamboman/mason.nvim" }
   use "williamboman/mason-lspconfig.nvim"
 
   -- Autocompletion
-  use "hrsh7th/nvim-cmp" -- Completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/nvim-cmp"                                                          -- Completion plugin
+  use "hrsh7th/cmp-buffer"                                                        -- buffer completions
+  use "hrsh7th/cmp-path"                                                          -- path completions
+  use "hrsh7th/cmp-cmdline"                                                       -- cmdline completions
+  use "saadparwaiz1/cmp_luasnip"                                                  -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
 
   -- Autotag
@@ -80,9 +80,9 @@ return require('packer').startup(function(use)
 
   -- File explorer
   use {
-    "kyazdani42/nvim-tree.lua", -- Nvim Tree, File explorer
+    "kyazdani42/nvim-tree.lua",                                                   -- Nvim Tree, File explorer
     requires = {
-      "kyazdani42/nvim-web-devicons", -- optional, for file icons
+      "kyazdani42/nvim-web-devicons",                                             -- optional, for file icons
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
   }
@@ -90,26 +90,26 @@ return require('packer').startup(function(use)
   use "MunifTanjim/prettier.nvim"
 
   -- Fuzzy Finder
-  use { -- Fuzzy Finder binary
+  use {                                                                           -- Fuzzy Finder binary
     'junegunn/fzf',
     run = function() vim.fn['fzf#install']() end
   }
-  use 'junegunn/fzf.vim' -- Fzf vim plugin
+  use 'junegunn/fzf.vim'                                                          -- Fzf vim plugin
 
-  use { 'windwp/nvim-autopairs', -- Auto closing for braces and quotes
+  use { 'windwp/nvim-autopairs',                                                  -- Auto closing for braces and quotes
     config = function() require("nvim-autopairs").setup {} end
   }
 
   use {
-    'nvim-lualine/lualine.nvim', -- Bottom status bar
+    'nvim-lualine/lualine.nvim',                                                  -- Bottom status bar
     requires = { 'kyazdani42/nvim-web-devicons', opt = true }
   }
   use {
-    "glepnir/lspsaga.nvim", -- UI for autocompletion, diagnostics, docs
+    "glepnir/lspsaga.nvim",                                                       -- UI for autocompletion, diagnostics, docs
     branch = "main"
   }
   use {
-    'nvim-treesitter/nvim-treesitter', -- Good 'ol treesitter for parsing programming languages
+    'nvim-treesitter/nvim-treesitter',                                            -- Good 'ol treesitter for parsing programming languages
     run = ':TSUpdate'
   }
   use {
@@ -125,19 +125,22 @@ return require('packer').startup(function(use)
     end
   }
 
+  -- Git signs
+  use "lewis6991/gitsigns.nvim"
+
   --Snippet
-  use 'L3MON4D3/LuaSnip' -- Snippet engine
+  use 'L3MON4D3/LuaSnip'                                                          -- Snippet engine
   use "rafamadriz/friendly-snippets"
 
   -- Colorschemes
-  use 'sainnhe/gruvbox-material' -- Colorscheme
+  use 'sainnhe/gruvbox-material'                                                  -- Colorscheme
   use { "ellisonleao/gruvbox.nvim" }
   use 'B4mbus/oxocarbon-lua.nvim'
   use "Shatur/neovim-ayu"
 
   -- Misc
-  use 'wakatime/vim-wakatime' -- Code stats
-  use "lewis6991/impatient.nvim" -- Plugin to load neovim faster
+  use 'wakatime/vim-wakatime'                                                     -- Code stats
+  use "lewis6991/impatient.nvim"                                                  -- Plugin to load neovim faster
   use "lukas-reineke/indent-blankline.nvim"
   -- Hop for easy vim motions
   use {
