f746769e7fd06c3952eaa526e273b1e55e3158b7
shiyaam <shiyaamsunderms@gmail.com>
2022-09-01 11:30:43 +0530

    feat: better highlight groups for colorschemes

--- lua/shiyaam/colorscheme.lua
+++ lua/shiyaam/colorscheme.lua
@@ -1,20 +1,32 @@
 local g = vim.g
--- local colorscheme = "ayu-mirage"
 
 -- Gruvbox
-g.gruvbox_material_better_performance = 1
 g.gruvbox_material_foreground="hard"
+g.gruvbox_material_better_performance = 1
 g.gruvbox_material_background="hard"
 g.gruvbox_material_disable_italic_comment=1
 g.gruvbox_material_diagnostic_text_highlight=1
 g.gruvbox_material_diagnostic_line_highlight=1
 local colorscheme = "gruvbox-material"
 
+
+
+-- Ayu Mirage
+-- local colorscheme = "ayu-mirage"
 -- g.ayuprefermirage=true
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+
+-- resetting highlight colors for good looking float windows
+vim.cmd ([[
+    try
+        colorscheme ]]..colorscheme .. [[ 
+    catch /^Vim\%((\a\+)\)\=:E185/
+        colorscheme default
+        set background=dark
+    endtry
+        highlight FloatBorder guibg=NONE ctermbg=NONE
+        highlight link NormalFloat Normal
+        highlight NormalFloat ctermbg=NONE guibg=NONE
+        highlight Pmenu ctermbg=NONE guibg=NONE
+]])
 
