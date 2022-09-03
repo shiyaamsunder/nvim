3c8bcff9403c9622a0e0cd135c4bca26e156b087
shiyaam <shiyaamsunderms@gmail.com>
2022-09-03 11:32:46 +0530

    feat: script for loading all plugins automatically

---
+++ lua/shiyaam/init.lua
@@ -0,0 +1,31 @@
+-- script for loading all lua files in this current directory
+-- https://github.com/hallettj/dot-vim/blob/master/home/.config/nvim/lua/init_d/init.lua
+
+
+local info = debug.getinfo(1, 'S')
+local module_directory = string.match(info.source, '^@(.*)/')
+local module_filename = string.match(info.source, '/([^/]*)$')
+
+local module_name = ... or "shiyaam"
+
+local function scandir(directory)
+  local i, t, popen = 0, {}, io.popen
+  local pfile = popen('ls -a "'..directory..'"')
+  for filename in pfile:lines() do
+    i = i + 1
+    t[i] = filename
+  end
+  pfile:close()
+  return t
+end
+
+local config_files = vim.tbl_filter(function (filename)
+  local is_lua_module = string.match(filename, "[.]lua$")
+  local is_this_file = filename == module_filename
+  return is_lua_module and not is_this_file
+end, scandir(module_directory))
+
+for i, filename in ipairs(config_files) do
+  local config_module = string.match(filename, "(.+).lua$")
+  require(module_name.."."..config_module)
+end
