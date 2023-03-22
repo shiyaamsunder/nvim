a3cb3a3f35656f428fcf23d8d96f899443157bda
Shiyaam Sunder <shiyaamsunder@users.noreply.github.com>
2023-03-22 08:30:25 +0530

    install script for depenedencies

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+echo "Installing ripgrep"
+sudo apt-get install ripgrep wget
+
+echo "export FZF_DEFAULT_COMMAND='rg --files'" >> ~/.bashrc 
+
+echo "Installing nvm"
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+
+exec bash
+echo "Installing LTS node version using nvm"
+
+
