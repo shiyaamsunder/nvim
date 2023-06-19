8fee7678a4b50a9598458b02018717bef446f4e1
shiyaam <shiyaamsunderms@gmail.com>
2023-06-19 21:58:18 +0530

    removed undodir

--- lua/shiyaam/config/options.lua
+++ lua/shiyaam/config/options.lua
@@ -9,7 +9,6 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
